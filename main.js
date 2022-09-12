99365701492378098cf84d043c8be55794a985b4
ilBarbogio <barbogioster.com>
2022-09-12 07:18:53 +0200

    Added readme and basic files

---
+++ main.js
@@ -0,0 +1,12 @@
+const can=document.querySelector("#main-canvas")
+const ctx=can.getContext("2d")
+
+const width=400
+const height=400
+
+function setupCanvas(){
+  can.width=width
+  can.height=height
+}
+
+setupCanvas()
---
+++ readme.md
@@ -0,0 +1,2 @@
+DAgrammer is born out of two necessities: honing my skills as a web developer
+and having an app built with Origami diagrams' designers in mind
